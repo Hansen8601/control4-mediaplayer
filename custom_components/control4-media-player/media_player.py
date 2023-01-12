67b80f9ad87d525a901af38ad20767f3d535ab00
Phil Hansen <Phil.hansen3@gmail.com>
2023-01-12 14:40:47 -0600

    Added source naming and mute functionality

--- custom_components/control4-media-player/media_player.py
+++ custom_components/control4-media-player/media_player.py
@@ -17,6 +17,7 @@ from homeassistant.components.media_player.const import (
     SUPPORT_TURN_ON,  
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_STEP,
+    SUPPORT_VOLUME_MUTE,
     SUPPORT_SELECT_SOURCE
 )
 
@@ -30,20 +31,24 @@ from homeassistant.const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-#Why is this needed? Does it initialize variables?
+#This sets the name used in configuration.yaml
 CONF_ON_VOLUME = "on_volume"
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_CHANNEL = "channel"
+CONF_SOURCE_LIST = "source_list"
+
 DEFAULT_PORT = 8750
 DEFAULT_VOLUME = 5
+DEFAULT_SOURCE_LIST = ['1','2','3','4']
 
 SUPPORT_CONTROL4 = (
-  SUPPORT_VOLUME_SET \
-  | SUPPORT_VOLUME_STEP \
-  | SUPPORT_TURN_ON \
-  | SUPPORT_TURN_OFF \
-  | SUPPORT_SELECT_SOURCE
+    SUPPORT_VOLUME_SET \
+    | SUPPORT_VOLUME_STEP \
+    | SUPPORT_TURN_ON \
+    | SUPPORT_TURN_OFF \
+    | SUPPORT_SELECT_SOURCE \
+    | SUPPORT_VOLUME_MUTE
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -52,7 +57,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_ON_VOLUME, default=DEFAULT_VOLUME): cv.positive_int,
         vol.Required(CONF_HOST): cv.string,
         vol.Required(CONF_CHANNEL): cv.positive_int,
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+        vol.Optional(CONF_SOURCE_LIST, default=DEFAULT_SOURCE_LIST): cv.ensure_list
     }
 )
 
@@ -65,29 +71,32 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
     channel = config.get(CONF_CHANNEL)
+    source_list = config.get(CONF_SOURCE_LIST)
 
-    async_add_entities([Control4MediaPlayer(entity_name, on_volume, host, port, channel)],)
+    async_add_entities([Control4MediaPlayer(entity_name, on_volume, host, port, channel,source_list)],)
 
 class Control4MediaPlayer(MediaPlayerEntity):
     #Research at https://developers.home-assistant.io/docs/core/entity/media-player/
     #_attr_device_class = 
 
-    def __init__(self, name, on_volume, host, port, channel):
+    def __init__(self, name, on_volume, host, port, channel,source_list):
         #self.hass = hass
         self._domain = __name__.split(".")[-2]
         self._name = name
         self._source = 1
-        self._source_list = ['1','2','3','4']
+        self._source_list = source_list
         self._on_volume = on_volume / 100
+        self._mute_volume = on_volume / 100
         self._state = STATE_OFF
         self._available = True
+        self._muted = False
         
         self._ampChannel = control4AmpChannel(host, port, channel)
 
     async def async_update(self):
         # Not sure if update(self) is required.
         _LOGGER.warn("update...")
-       
+
     @property
     def should_poll(self):
         return False
@@ -96,6 +105,10 @@ class Control4MediaPlayer(MediaPlayerEntity):
     def icon(self) -> str | None:
         """Return the icon."""
         return "mdi:speaker"
+    
+    @property
+    def is_volume_muted(self):
+        return self._muted
 
     @property
     def name(self):
@@ -125,11 +138,25 @@ class Control4MediaPlayer(MediaPlayerEntity):
         """Flag media player features that are supported."""
         return SUPPORT_CONTROL4
 
+    async def async_mute_volume(self, mute: bool) -> None:
+        """Mute (true) or unmute (false) media player."""
+        if mute:
+            self._muted = True
+            self._mute_volume = self._ampChannel.volume
+            self._ampChannel.volume  = 0 
+            _LOGGER.warn("volume set to  zero to mute")
+        else:
+            self._muted = False
+            self._ampChannel.volume  = self._mute_volume 
+            _LOGGER.warn("volume set to pre-mute level")
+        self.schedule_update_ha_state()
+
     async def async_select_source(self,source):
         self._source = source
-        self._ampChannel.source = source
+        self._ampChannel.source = self._source_list.index(source) + 1
         self.schedule_update_ha_state()
-        _LOGGER.warn("Source set to " + str(self._ampChannel.source))
+        _LOGGER.warn("Source index is " + str(self._source_list.index(source) + 1))
+        _LOGGER.warn("Source set to " + str(self._source))
 
     async def async_turn_on(self):
         _LOGGER.warn("Turning on...")
@@ -144,7 +171,7 @@ class Control4MediaPlayer(MediaPlayerEntity):
         result = self._ampChannel.turn_off()
         self._state = STATE_OFF 
         self.schedule_update_ha_state()
-       
+
     async def async_volume_up(self):
         self._ampChannel.volume = self._ampChannel.volume + .01
         self.schedule_update_ha_state()
